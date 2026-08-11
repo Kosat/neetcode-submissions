f3f1de585a3eadcf988e4e576fd427e998a9f11c
Constantine <kostikkv@gmail.com>
2026-08-10 21:26:58 -0400

    Add: sort-an-array - submission-2

---
+++ Data Structures & Algorithms/sort-an-array/submission-2.cs
@@ -0,0 +1,56 @@
+// My Merge Sort. My own implementation from mery. The next day after I was following the guide.
+public class Solution {
+    public int[] SortArray(int[] nums) {
+        return MergeSort(nums, 0, nums.Length - 1);
+    }
+
+    private int[] MergeSort(int[] n, int l, int r) {
+
+        if(l == r) {
+            return n;
+        }
+
+        int m = l + (r - l)/2;
+
+        MergeSort(n, l, m);
+        MergeSort(n, m + 1, r);
+
+        MergeTwoArrays(n, l, m, r);
+
+        return n;
+    }
+
+    // In-pace merging, zipping 2 arrays. 1 array is to the left from 'm', and the second array is to the right from 'm'.
+    private void MergeTwoArrays(int[] n, int l, int m, int r) {
+        int[] left = n[l..(m+1)];
+        int[] right = n[(m+1)..(r+1)];
+
+        int i=l, j=0, k=0;
+
+        while(j < left.Length && k < right.Length) {
+            if(left[j] < right[k]) {
+                n[i] = left[j];
+                j++;
+            } else {
+                n[i] = right[k];
+                k++;
+            }
+            i++;
+        }
+
+        // process the remaing left array elements
+        while(j < left.Length) {
+            n[i] = left[j];
+            j++;
+            i++;
+        }
+
+        // process the remaing right array elements
+        while(k < right.Length){
+            n[i] = right[k];
+            k++;
+            i++;
+        }
+
+    }
+}
