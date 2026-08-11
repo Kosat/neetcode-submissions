b6b94b954190432769e1ccf0b93999de104bd32c
Constantine <kostikkv@gmail.com>
2026-08-10 23:05:24 -0400

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.cs
@@ -0,0 +1,34 @@
+// I made a mistake by introducing an aux class and placing it as values into both Dic and Queue and then updating Freqency in that class. But it did not work b/c PriorityQueue does not dynaimcally updates itsef.
+// KIMPORTANT: This problem can also be solved with using extra List<int>[] instead of PriorityQueue
+public class Solution {
+     public int[] TopKFrequent(int[] nums, int k) {
+        var counts = new Dictionary<int, int>();
+
+        foreach (int n in nums)
+        {
+            if (counts.ContainsKey(n))
+                counts[n]++;
+            else
+                counts[n] = 1;
+        }
+
+        var priorityQueue = new PriorityQueue<int, int>(
+            Comparer<int>.Create((a, b) => b.CompareTo(a))
+        );
+        
+        foreach (var entry in counts)
+        {
+            // Higher frequency should have higher priority.
+            priorityQueue.Enqueue(entry.Key, entry.Value);
+        }
+
+        var result = new int[k];
+
+        for (int i = 0; i < k; i++)
+        {
+            result[i] = priorityQueue.Dequeue();
+        }
+
+        return result.ToArray();
+    }
+}
