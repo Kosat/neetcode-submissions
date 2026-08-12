938e78299d5a2ad408775294e36a4d7f22bb9d04
Constantine <kostikkv@gmail.com>
2026-08-12 15:39:02 -0400

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.cs
@@ -0,0 +1,52 @@
+// My first attempt, after Perplexity-assisted fixes
+// Mistakes with Sorting by TimeToTarget instead of Position DESC
+// Mistake when calcualting TimeToTarget as Integer instead of Double
+
+public class Solution {
+
+    class Car {
+        public int Position;
+        public int Speed;
+        public double TimeToTarget;
+    }
+
+    public int CarFleet(int target, int[] position, int[] speed) {
+        
+        int carsCount = position.Length;
+        
+        List<Car> carsSortedByTTT = [];
+
+        for(int i=0; i < carsCount; i++) {
+            carsSortedByTTT.Add(
+                new Car() { 
+                    Position = position[i],
+                    Speed = speed[i],
+                    TimeToTarget = ((double)target - position[i])/speed[i],
+                }
+            );
+        }
+
+        var carsComparer = Comparer<Car>.Create((Car c1, Car c2) => c2.Position.CompareTo(c1.Position));
+        carsSortedByTTT.Sort(carsComparer);
+
+        Stack<Car> cars = [];
+        int fleetsCount = 0;
+        foreach (var car in carsSortedByTTT) {
+            if(cars.Count == 0) {
+                cars.Push(car);
+                fleetsCount++;
+            } else {
+                if(car.TimeToTarget <= cars.Peek().TimeToTarget) {
+                    // This car joins the existing fleet
+                } else {
+                    // This car will not be able to catch-up the next-faster car in the stack
+                    cars.Push(car);
+                    fleetsCount++;
+                }
+
+            }
+        }     
+
+        return fleetsCount;
+    }
+}
