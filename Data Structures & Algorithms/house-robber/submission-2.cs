7005addc97feb14fd983aa87b70c731fc992d0e8
Constantine <kostikkv@gmail.com>
2026-09-03 19:49:49 -0400

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int Rob(int[] nums)
+        => RobHelper(nums);
+
+    private int RobHelper(int[] nums)
+    {
+        int n = nums.Length;
+
+        if (n == 0) return 0;
+
+        int[] dp = new int[n + 2];
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = Math.Max(0 + dp[i - 1], nums[i - 1] + dp[i - 2]);
+        }
+
+        return dp[n];
+    }
+}
