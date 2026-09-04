e5a38f159d26e5ac6fa9dcddb2dd247a506bcd9b
Constantine <kostikkv@gmail.com>
2026-09-04 11:03:41 -0400

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.cs
@@ -0,0 +1,32 @@
+public class Solution {
+
+    // Naive sub-optimal solution
+    public int[] CountBits(int n) {
+        
+
+        List<int> bitsCount = [];
+        for (int i = 0; i <= n; i++)
+        {
+            bitsCount.Add(CountBitsForOneNumber(i));
+        }
+
+        return bitsCount.ToArray();
+    }
+
+    private int CountBitsForOneNumber(int n)
+    {
+        int bitsCount = 0;
+        int mask = 1;
+
+        for(int i = 0; i < 32; i++)
+        {
+            if((n & mask) == mask)
+            {
+                bitsCount++;
+            }
+            mask <<= 1;
+        }
+
+        return bitsCount;
+    }
+}
