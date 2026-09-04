5db793d6924cfd5d4dfae89f6c3169c0467e75d2
Constantine <kostikkv@gmail.com>
2026-09-04 18:07:39 -0400

    Add: sum-of-two-integers - submission-1

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-1.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int GetSum(int a, int b) {
+        
+        int sum = 0;
+        int i = 0;
+        int carry = 0;
+        while (i < 32)
+        {
+            int curA = a & 1;
+            int curB = b & 1;
+
+            sum |= (curA ^ curB ^ carry) << i;
+
+            // (a_bit + b_bit + carry) >= 2 
+            if ((curA ^ curB ^ carry) == 0 && !(curA == 0 && curB == 0 && carry == 0) || (curA == 1 && curB == 1 && carry == 1))
+            {
+                carry = 1;
+            }
+            else
+            {
+                carry = 0;
+            }
+
+            a >>= 1;
+            b >>= 1;
+            i++;
+        }
+
+        return sum;
+    }
+}
