f322bbb5a8a21529755b580249cc6fca46df4496
Constantine <kostikkv@gmail.com>
2026-09-06 13:47:57 -0400

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int Reverse(int x) {
+        int result = 0;
+        while (x != 0)
+        {
+            int digit = x % 10;
+            x /= 10;
+
+            // Overflow check (before appending the digit)
+            if (result > int.MaxValue / 10 ||
+                (result == int.MaxValue / 10 && digit > int.MaxValue % 10))
+            {
+                return 0;
+            }
+            if (result < int.MinValue / 10 ||
+                (result == int.MinValue / 10 && digit < int.MinValue % 10))
+            {
+                return 0;
+            }
+            result = result * 10 + digit;
+        }
+        
+        return result;
+    }
+}
