3688d6429bad2031bae083ed7d7ba145e93b0203
Constantine <kostikkv@gmail.com>
2026-09-06 17:57:38 -0400

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.cs
@@ -0,0 +1,69 @@
+public class Solution {
+    public string LongestPalindrome(string s) {
+        string result = string.Empty;
+
+        int n = s.Length;
+
+        // Odd middle point case
+        int m = 0; // the middle pointer
+        int l, r; // left and right pointers
+
+        var ExpandHelper = (int l, int r) =>
+        {
+            while (l >= 0 && r <= n - 1 && s[l] == s[r])
+            {
+                int curPalindromeLen = r - l + 1;
+                if (curPalindromeLen > result.Length)
+                {
+                    result = s[l..(r + 1)];
+                }
+
+                l--;
+                r++;
+            }
+        };
+
+        for (; m < n; m++)
+        {
+            l = r = m;
+            ExpandHelper(l, r);
+            // while (l >= 0 && r <= n - 1 && s[l] == s[r])
+            // {
+            //     int curPalindromeLen = r - l + 1;
+            //     if (curPalindromeLen > result.Length)
+            //     {
+            //         result = s[l..(r + 1)];
+            //     }
+
+            //     l--;
+            //     r++;
+            // }
+        }
+
+
+        // Even middle point case
+        l = 0;
+        m = 1;
+        r = 1;
+
+        for (; m < n; m++)
+        {
+            l = m - 1;
+            r = m;
+            while (l >= 0 && r <= n - 1 && s[l] == s[r])
+            {
+                int curPalindromeLen = r - l + 1;
+                if (curPalindromeLen > result.Length)
+                {
+                    result = s[l..(r + 1)];
+                }
+
+                l--;
+                r++;
+            }
+        }
+
+
+        return result;
+    }
+}
