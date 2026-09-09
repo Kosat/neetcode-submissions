b0520b2e6f19f78d9d65d513bb1a4484faf7426d
Constantine <kostikkv@gmail.com>
2026-09-08 22:17:32 -0400

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public bool IsHappy(int n) {
+        var GetSumOfDigitSquares = (int n) =>
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                int digit = n % 10;
+                n /= 10;
+                sum += digit * digit;
+            }
+            return sum;
+        };
+
+        HashSet<int> visited = [];
+
+        while (true) 
+        {
+            visited.Add(n);
+
+            int sum = GetSumOfDigitSquares(n);
+            if (sum == 1)
+            {
+                return true;
+            }
+
+            // Cycle detection
+            // Repeat the above step until the number equals 1, or it loops infinitely in a cycle which does not include 1.
+            if(visited.Contains(sum))
+            {
+                return false;
+            }
+
+            n = sum;
+        }
+
+        return false;
+    }
+}
